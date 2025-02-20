58fa723e954781eb22b7d834abb277b908f65644
Alex S <insten490@gmail.com>
2025-02-20 12:21:31 -0600

    added more methods to Library class

--- classes/Library.py
+++ classes/Library.py
@@ -8,6 +8,29 @@ class Library:
             raise ValueError("Book must be an instance of Book class.")
         else:
             self.books.append(book)
+    
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                break
+        else:
+            print(f"Book '{title}' not found in the library.")
+
+    def display_books(self):
+        if self.books:
+            for book in self.books:
+                print(book)
+        else:
+            print("No books are stored in the library.")
+    
+    def search_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                return book
+        else:
+            print(f"Book '{title}' not found in the library.")
+
 
 class Book:
     def __init__(self, title, author, isbn):
